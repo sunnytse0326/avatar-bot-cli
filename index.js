2b20bf7ec439601114b543ceb700832c94c6c9af
Sunny <sunnytse0326@gmail.com>
2019-09-09 22:46:36 +0800

    init action command

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
 const chalk = require('chalk')
 
-module.exports = () => {
-   console.log(chalk.green('*************************************************************************\n\n\t\t\tWelcome to Avator-CLI\n\n\tAn application which provides a grid environment to test your\n\tcharacter\'s capability from dedicated commands.\n\n*************************************************************************\n\n'))
+const helpers = require('./utils/helpers')
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const errorMessages = {
+  INVALID_INPUT: 'Please input valid commands',
+  CORRECT_PLACE_ARGUMENTS: 'Please input correct PLACE arguments',
+  CORRECT_PLACE_LOCATION: 'Avatar location must be in numeric format',
+  CORRECT_PLACE_FACING: 'Avatar facing direction must be in \'N\', \'E\', \'S\', \'W\' characters',
+}
+
+const askQuestion = {
+  PLACE_AVATAR: 'Place your first avator: ',
+  NEXT_ACTION: 'What is the next action? '
+}
+
+module.exports = () => {  
+  displayTopic()
+
+  determineAction(askQuestion.PLACE_AVATAR)
+}
+
+let displayTopic = () => {
+   console.log(chalk.green('*************************************************************************\n\n\t\t\tWelcome to Avator-CLI\n\n\tAn application which provides a grid environment to test\n\tyour character\'s capability from dedicated commands.\n\n*************************************************************************\n\nUasge:\n  - PLACE {X} {Y} {F}:\n    Place an avatar with initial position and facing direction.\n  - MOVE:\n    Move the avatar with one unit step in current facing direction\n  - REPORT:\n    Show the avator current location\n\nExample:\n\nPLACE 0 0 N\n(Placing avatar to the x-position in 0 and y-position in 0 and facing\ndirection to North) \n\n'))
+}
+
+let determineAction = (question) => {
+  readline.question(question, (input) => {
+    const args = input.split(' ')
+    console.log(args[0])
+
+    switch(args[0]){
+      case 'PLACE':
+        if(args.length < 4){
+          console.log(chalk.red(errorMessages.CORRECT_PLACE_ARGUMENTS))
+          determineAction(question)
+        } else if(!helpers.isNumeric(args[1]) || !helpers.isNumeric(args[2])){
+          console.log(chalk.red(errorMessages.CORRECT_PLACE_LOCATION))
+          determineAction(question)
+        }
+      break
+      case 'MOVE':
+      break
+      case 'REPORT':
+      break
+      default:
+        console.log(chalk.red(errorMessages.INVALID_INPUT))
+        determineAction(question)
+      break
+    }
+  })
 }
