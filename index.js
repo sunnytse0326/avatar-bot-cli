001a266d352d9380ea87724624094a270cb1254c
Sunny <sunnytse0326@gmail.com>
2019-09-09 20:53:58 +0800

    init proj arch

---
+++ index.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk')
+
+module.exports = () => {
+   console.log(chalk.green('*************************************************************************\n\n\t\t\tWelcome to Avator-CLI\n\n\tAn application which provides a grid environment to test your\n\tcharacter\'s capability from dedicated commands.\n\n*************************************************************************\n\n'))
+}
